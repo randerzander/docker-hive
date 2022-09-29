272b37937a6c2dbc14bdffaa714e618391b224aa
Randy Gelhausen <rgelhau@gmail.com>
2022-09-29 11:01:20 -0700

    Actually add code for attempting to create a Hive table in dask-sql

--- dask_sql_test.py
+++ dask_sql_test.py
@@ -1,4 +1,10 @@
 from pyhive import hive
+from dask_sql import Context
 cursor = hive.connect('hive-server').cursor()
 cursor.execute('SELECT * FROM testdb.employee')
+
+# validates container's ability to query from hive
 print(cursor.fetchall())
+
+c = Context()
+c.create_table("employee", cursor, hive_table_name="testdb.employee")
